2b288f18043e9538d9676052f0fd3e2e847552c4
shayaliza <magicalmind01@gmail.comgit statuss>
2024-06-08 23:46:25 +0530

    tanstack shadcn and installation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,34 @@
-// import React from "react";
-// import Navbar from "./navbar";
-// import Search from "./search";
-// import Rose from "./rose";
-
-// const App: React.FC = () => {
-//   return (
-//     <div className="flex flex-col h-screen overflow-hidden">
-//       <Navbar />
-//       <div className="flex-grow flex items-start justify-center mt-4">
-//         <Search />
-//       </div>
-//       <Rose />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import Navbar from "./navbar";
 import Search from "./search";
 import Rose from "./rose";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { Toaster } from "@/components/ui/toaster";
+
+const queryClient = new QueryClient();
 const App: React.FC = () => {
   return (
-    <div className="h-screen overflow-hidden w-full">
-      <Navbar />
-      <Search />
-      <div className="relative">
-        <div className="absolute top-0 left-0 ml-10 pt-10">
-          <div className="h-12"></div>
-          <div className="text-4xl font-bold pt-10 text-green-700 font-quicksand">
-            Scrape
+    <QueryClientProvider client={queryClient}>
+      <div className="h-screen overflow-hidden w-full">
+        <Navbar />
+        <Search />
+        <div className="relative">
+          <div className="absolute top-0 left-0 ml-10 pt-10">
+            <div className="h-12"></div>
+            <div className="text-4xl font-bold pt-10 text-green-700 font-quicksand">
+              Scrape
+            </div>
+            <div className="text-6xl font-extrabold text-pink-600 pt-4 font-quicksand">
+              With Love
+            </div>
           </div>
-          <div className="text-6xl font-extrabold text-pink-600 pt-4 font-quicksand">
-            With Love
-          </div>
+          <Rose />
         </div>
-        <Rose />
+        <Toaster />
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
